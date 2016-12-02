c4b1e203ef33b6928de36653223ee6d7f83074c7
Krasimir Chariyski <kr.chariyski@gmail.com>
2016-12-03 00:18:21 +0200

    quick fix

--- others/le_renew.sh
+++ others/le_renew.sh
@@ -6,5 +6,5 @@ echo "Good for now"
 else
 # Launching certbot from installed location with HTTPS challenge on port 8123 (port your HA usually running on)
 # Stopping HA service first and running after update
-sudo /home/pi/certbot/certbot-auto renew --quiet --no-self-upgrade --standalone --preferred-challenges tls-sni-01 --tls-sni-01-port 8123 --pre-hook "systemctl stop home-assistant@rootservice" --post-hook "systemctl start home-assistant@root.service"
+sudo /home/pi/certbot/certbot-auto renew --quiet --no-self-upgrade --standalone --preferred-challenges tls-sni-01 --tls-sni-01-port 8123 --pre-hook "systemctl stop home-assistant.service" --post-hook "systemctl start home-assistant.service"
 fi
